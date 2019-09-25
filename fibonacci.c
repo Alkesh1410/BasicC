1511d96e06dac0a3e299233060b6144db6902c50
Alkesh Shah <30997774+Alkesh1410@users.noreply.github.com>
2019-09-25 17:33:26 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+//	Prepared by Alkesh Shah
+
+#include<stdio.h>
+int main()
+{
+	printf("Please enter the length of Fibonacci\t:");
+	int length;
+	scanf("%d",&length);
+
+	int first = 0, second = 1;
+	int nextValue;
+
+	for (int i = 1; i <=length ; ++i)
+	{
+		printf("%d ", first);
+		nextValue = first + second;
+		first = second;
+		second = nextValue;
+	}
+	printf("\n");
+}
---
+++ fibonacciRecursion.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int computeFibonacci(int);
+
+int main()
+{
+	printf("Please enter the length of fibonacci\t:");
+	int length;
+	scanf("%d",&length);
+
+	if(length < 0)
+		printf("Can not compute fibonacci in opposite direction");
+	else
+		for(int i = 0; i<length; i++)
+			printf("%d ",computeFibonacci(i));
+	
+	printf("\n");
+	return 0;
+}
+
+int computeFibonacci(int number)
+{
+	if (number == 0)
+		return 0;
+	else if (number == 1)
+		return 1;
+	else
+		return computeFibonacci(number-1) + computeFibonacci(number-2);
+}
