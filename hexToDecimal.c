5f31b3385a4705adac9d7e624aff4d1d57e06242
Alkesh Shah <30997774+Alkesh1410@users.noreply.github.com>
2019-09-26 12:25:41 +0530

    Add files via upload

---
+++ hexToDecimal.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	printf("Please enter the number in hex\t:");
+	char number[7];
+	scanf("%s",number);
+	
+	int length = strlen(number) - 1;
+	int newNumber = atoi(number);
+	int decimalValue = 0;
+
+	for(int i=0; number[i]; i++)
+	{
+		int value;
+		if(number[i]>='0' && number[i]<='9')
+			value = number[i] - 48;
+		else if(number[i]>='a' && number[i]<='f')
+			value = number[i] - 97 + 10;
+		else if(number[i]>='A' && number[i]<='F')
+			value = number[i] - 65 + 10;
+
+		decimalValue += value * pow(16, length);
+		length--;
+	}
+//	for(int i = 0; newNumber; i++,newNumber=newNumber/10)
+//	{
+//		int rem = newNumber % 10;
+//		decimalValue = (decimalValue + rem*pow(16,i));
+//	}
+
+	printf("The converted value for entered hex number is %d\n",decimalValue);
+
+	return 0;
+}
